ac835817d6fb56a9bb562d2f08063d7f16b6f481
dmitryprotasov-gif <dmitry.protasov@indriver.com>
2026-07-27 03:42:57 +0300

    align cabinet with Audio2MIDI site

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@fontsource-variable/montserrat'
 import App from './App'
 import './styles/global.css'
+import './styles/cabinet.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
